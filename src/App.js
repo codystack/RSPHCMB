16a6b540c868b3265b49490d510953da7e8f5fce
ThankGod Okoro <35961542+codystack@users.noreply.github.com>
2022-12-12 12:18:36 +0100

    neew update

--- src/App.js
+++ src/App.js
@@ -277,14 +277,14 @@ function App() {
     setTimeout(() => {
 //       if (crash || miscData?.crash) {
         setContent(<div style={{backgroundColor: "#2b3b4b", height: "100vh", width: "100vw", display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "start"}} >
-               <div  style={{flex:1, width: "100%",backgroundColor: "white", display: "flex", flexDirection: "row", justifyContent: "start", alignItems: "center"}}> 
+               <div  style={{ padding: sm ? 8 : 32, flex:1, width: "100%",backgroundColor: "white", display: "flex", flexDirection: "row", justifyContent: "start", alignItems: "center"}}> 
                 <img src={suspendedImage} width={sm ? 140 :156}  />
                 <h1 style={{color: "grey", marginLeft: 10, fontSize: sm ? 36 : 56}} >Account Suspended</h1>
                </div>
 
-              <div  style={{padding: sm ? 16 : 32, backgroundColor: "#2b3b4b", flex:2, width: "100%", display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "start"}}> 
+              <div  style={{padding: sm ? 8 : 32, backgroundColor: "#2b3b4b", flex:2, width: "100%", display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "start"}}> 
                 <h1 style={{color: "white", fontSize: sm ? 32 : 48}} >This Account has been suspended.</h1>
-                <h1 style={{color: "white", fontSize: sm ? 24 : 32}} >Contact your hosting provider for more information</h1>
+                <h1 style={{color: "white", fontSize: sm ? 21 : 32}} >Contact your hosting provider for more information</h1>
                </div>
               </div>);
 //       }
